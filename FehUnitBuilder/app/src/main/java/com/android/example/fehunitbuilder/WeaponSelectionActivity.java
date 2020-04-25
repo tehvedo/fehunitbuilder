911f18a0e3a886367bb99b3c2524dad58f1b3021
tehvedo <57195501+tehvedo@users.noreply.github.com>
2020-04-25 17:38:46 -0400

    Created Activities for Skill Selection
    They are just empty, unused activities for now. Need to figure out how to access the proper skills from the JSON for each activity and populate a recyclerlist to select from.

---
+++ FehUnitBuilder/app/src/main/java/com/android/example/fehunitbuilder/WeaponSelectionActivity.java
@@ -0,0 +1,13 @@
+package com.android.example.fehunitbuilder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WeaponSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weapon_selection);
+    }
+}
